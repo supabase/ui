dc2c20321bef5f5953aaf8b3d6e05912a268d9ce
mildtomato <jon.summers.muir@googlemail.com>
2021-01-07 16:13:29 +0800

    chore: add `iconRight` inline prop to <Button>
    both icons are hidden with `loading` is true

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -11,6 +11,7 @@ export default {
 export const Default = (args: any) => <Button {...args}>Button text</Button>
 export const withStyles = (args: any) => <Button {...args}>Button text</Button>
 export const withIcon = (args: any) => <Button {...args}>Button text</Button>
+export const withIconRight = (args: any) => <Button {...args}>Button text</Button>
 export const withBlock = (args: any) => <Button {...args}>Button text</Button>
 export const withOnlyIcon = (args: any) => <Button {...args}/>
 export const withOnlyLoading = (args: any) => <Button {...args}/>
@@ -20,6 +21,11 @@ const icon = <Icon type={"Package"}/>
 withIcon.args = {
   type: 'primary',
   icon: icon,
+}
+
+withIconRight.args = {
+  type: 'primary',
+  iconRight: <Icon type={"ChevronRight"} strokeWidth={2}/>,
 }
 
 withStyles.args = {
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,18 +6,27 @@ import { Transition, Icon } from '../../index'
 import { IconContext } from '../Icon/IconContext'
 
 interface Props {
-  block : boolean
-  className : any
+  block: boolean
+  className: any
   children: React.ReactNode
   disabled: boolean
   onClick?: React.MouseEventHandler<HTMLButtonElement>
-  icon? : React.ReactNode
+  icon?: React.ReactNode
+  iconRight?: React.ReactNode
   loading?: boolean
   shadow?: boolean
   size?: 'tiny' | 'small' | 'medium' | 'large' | 'xlarge'
-  style? : React.CSSProperties
-  type?: 'primary' | 'default' | 'secondary' | 'outline' | 'dashed' | 'link' | 'text'
+  style?: React.CSSProperties
+  type?:
+    | 'primary'
+    | 'default'
+    | 'secondary'
+    | 'outline'
+    | 'dashed'
+    | 'link'
+    | 'text'
   danger: boolean
+  spaceSize: number
 }
 
 const Button = ({
@@ -27,33 +36,29 @@ const Button = ({
   disabled = false,
   onClick,
   icon,
+  iconRight,
   loading = false,
   shadow = true,
   size = 'medium',
   style,
   type = 'primary',
   danger,
-  ...props
-} : Props) => {
-  // default classes
+  spaceSize,
+}: Props) => {
   let classes = []
   let containerClasses = ['sbui-btn-container']
-
-  // if (type !== 'ghost' && shadow) {
-  //   containerClasses.push('sbui-btn--shadow')
-  // }
 
   if (block) {
     containerClasses.push('sbui-btn--w-full')
     classes.push('sbui-btn--w-full')
   }
 
-  if (icon) {
-    classes.push('sbui-btn--with-icon')
+  if (danger) {
+    classes.push('sbui-btn--danger')
   }
 
-  if (danger ) {
-    classes.push('sbui-btn--danger')
+  if (shadow) {
+    classes.push('sbui-btn-container--shadow')
   }
 
   if (size) {
@@ -76,28 +81,24 @@ const Button = ({
           style={style}
           type="button"
         >
-          {showIcon && (
-          <Transition
-            show={showIcon ? true : false}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
-            {loading ? (
+          {showIcon &&
+            (loading ? (
               <Icon
                 size={size}
                 className={'sbui-btn--anim--spin'}
                 type={'Loader'}
               />
             ) : icon ? (
-              <IconContext.Provider value={{contextSize: size}}>{icon}</IconContext.Provider>
-            ) : null}
-          </Transition>
-          )}
+              <IconContext.Provider value={{ contextSize: size }}>
+                {icon}
+              </IconContext.Provider>
+            ) : null)}
           {children && <span>{children}</span>}
+          {iconRight && !loading && (
+            <IconContext.Provider value={{ contextSize: size }}>
+              {iconRight}
+            </IconContext.Provider>
+          )}
         </button>
       </span>
     </React.Fragment>
