528dc92a1e562962a0eec4804de21a20f07f6224
mildtomato <jon.summers.muir@googlemail.com>
2021-01-13 13:39:57 +0800

    fix: change primary button bg color to a darker brand shade and drop shadow attribute changed from inner shadow effect to drop shadow on all button text
    fixes #59

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,22 +2,139 @@ import React, { useRef, useState } from 'react'
 
 import { Button } from '.'
 import { Icon } from '../Icon'
+import { Space } from '../Space'
 
 export default {
   title: 'General/Button',
   component: Button,
 }
 
-export const Default = (args: any) => <Button {...args}>Button text</Button>
-export const withStyles = (args: any) => <Button {...args}>Button text</Button>
-export const withIcon = (args: any) => <Button {...args}>Button text</Button>
-export const withIconRight = (args: any) => <Button {...args}>Button text</Button>
+export const all = (args: any) => (
+  <>
+    <Space direction="vertical" size={6}>
+      <Space>
+        <Button size="tiny">Button text</Button>
+        <Button size="tiny" type="secondary">
+          Button text
+        </Button>
+        <Button size="tiny" type="default">
+          Button text
+        </Button>
+        <Button size="tiny" type="link">
+          Button text
+        </Button>
+        <Button size="tiny" type="text">
+          Button text
+        </Button>
+        <Button size="tiny" type="dashed">
+          Button text
+        </Button>
+        <Button size="tiny" type="outline">
+          Button text
+        </Button>
+      </Space>
+
+      <Space>
+        <Button size="small">Button text</Button>
+        <Button size="small" type="secondary">
+          Button text
+        </Button>
+        <Button size="small" type="default">
+          Button text
+        </Button>
+        <Button size="small" type="link">
+          Button text
+        </Button>
+        <Button size="small" type="text">
+          Button text
+        </Button>
+        <Button size="small" type="dashed">
+          Button text
+        </Button>
+        <Button size="small" type="outline">
+          Button text
+        </Button>
+      </Space>
+
+      <Space>
+        <Button>Button text</Button>
+        <Button size="medium" type="secondary">
+          Button text
+        </Button>
+        <Button size="medium" type="default">
+          Button text
+        </Button>
+        <Button size="medium" type="link">
+          Button text
+        </Button>
+        <Button size="medium" type="text">
+          Button text
+        </Button>
+        <Button size="medium" type="dashed">
+          Button text
+        </Button>
+        <Button size="medium" type="outline">
+          Button text
+        </Button>
+      </Space>
+
+      <Space>
+        <Button size="large">Button text</Button>
+        <Button size="large" type="secondary">
+          Button text
+        </Button>
+        <Button size="large" type="default">
+          Button text
+        </Button>
+        <Button size="large" type="link">
+          Button text
+        </Button>
+        <Button size="large" type="text">
+          Button text
+        </Button>
+        <Button size="large" type="dashed">
+          Button text
+        </Button>
+        <Button size="large" type="outline">
+          Button text
+        </Button>
+      </Space>
+      <Space>
+        <Button size="xlarge">Button text</Button>
+        <Button size="xlarge" type="secondary">
+          Button text
+        </Button>
+        <Button size="xlarge" type="default">
+          Button text
+        </Button>
+        <Button size="xlarge" type="link">
+          Button text
+        </Button>
+        <Button size="xlarge" type="text">
+          Button text
+        </Button>
+        <Button size="xlarge" type="dashed">
+          Button text
+        </Button>
+        <Button size="xlarge" type="outline">
+          Button text
+        </Button>
+      </Space>
+    </Space>
+  </>
+)
+export const Default = (args: any) => <Button>Button text</Button>
+export const withStyles = (args: any) => <Button>Button text</Button>
+export const withIcon = (args: any) => <Button>Button text</Button>
+export const withIconRight = (args: any) => (
+  <Button {...args}>Button text</Button>
+)
 export const withBlock = (args: any) => <Button {...args}>Button text</Button>
-export const withOnlyIcon = (args: any) => <Button {...args}/>
-export const withOnlyLoading = (args: any) => <Button {...args}/>
-export const withRef = () => { 
+export const withOnlyIcon = (args: any) => <Button {...args} />
+export const withOnlyLoading = (args: any) => <Button {...args} />
+export const withRef = () => {
   const buttonRef = useRef(null)
-  const [msg, setMsg] = useState("Click button to console.log Ref")
+  const [msg, setMsg] = useState('Click button to console.log Ref')
 
   function onClick() {
     const message = `container: ${buttonRef?.current.container} button:${buttonRef?.current.button}  `
@@ -26,17 +143,17 @@ export const withRef = () => {
   }
 
   return (
-  <>
-    <Button ref={buttonRef} onClick={onClick}>
-      Button with forwardRef
-    </Button>
+    <>
+      <Button ref={buttonRef} onClick={onClick}>
+        Button with forwardRef
+      </Button>
 
-    <p style={{color: '#666666'}}>{msg}</p>
-  </>
+      <p style={{ color: '#666666' }}>{msg}</p>
+    </>
   )
 }
 
-const icon = <Icon type={"Package"}/>
+const icon = <Icon type={'Package'} />
 
 withIcon.args = {
   type: 'primary',
@@ -45,23 +162,23 @@ withIcon.args = {
 
 withIconRight.args = {
   type: 'primary',
-  iconRight: <Icon type={"ChevronRight"} strokeWidth={2}/>,
+  iconRight: <Icon type={'ChevronRight'} strokeWidth={2} />,
 }
 
 withStyles.args = {
   type: 'primary',
-  style: {backgroundColor: 'red', color: 'yellow'}
+  style: { backgroundColor: 'red', color: 'yellow' },
 }
 
 withBlock.args = {
   type: 'primary',
-  block: true
+  block: true,
 }
 
 withOnlyIcon.args = {
-  icon: icon
+  icon: icon,
 }
 
 withOnlyLoading.args = {
-  loading: true
+  loading: true,
 }
