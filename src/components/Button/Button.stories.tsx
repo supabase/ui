14d70e4724d5eb74a5b0715122baae7509f0805c
mildtomato <jon.summers.muir@googlemail.com>
2021-01-13 13:40:59 +0800

    reset button stories file

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,130 +2,15 @@ import React, { useRef, useState } from 'react'
 
 import { Button } from '.'
 import { Icon } from '../Icon'
-import { Space } from '../Space'
 
 export default {
   title: 'General/Button',
   component: Button,
 }
 
-export const all = (args: any) => (
-  <>
-    <Space direction="vertical" size={6}>
-      <Space>
-        <Button size="tiny">Button text</Button>
-        <Button size="tiny" type="secondary">
-          Button text
-        </Button>
-        <Button size="tiny" type="default">
-          Button text
-        </Button>
-        <Button size="tiny" type="link">
-          Button text
-        </Button>
-        <Button size="tiny" type="text">
-          Button text
-        </Button>
-        <Button size="tiny" type="dashed">
-          Button text
-        </Button>
-        <Button size="tiny" type="outline">
-          Button text
-        </Button>
-      </Space>
-
-      <Space>
-        <Button size="small">Button text</Button>
-        <Button size="small" type="secondary">
-          Button text
-        </Button>
-        <Button size="small" type="default">
-          Button text
-        </Button>
-        <Button size="small" type="link">
-          Button text
-        </Button>
-        <Button size="small" type="text">
-          Button text
-        </Button>
-        <Button size="small" type="dashed">
-          Button text
-        </Button>
-        <Button size="small" type="outline">
-          Button text
-        </Button>
-      </Space>
-
-      <Space>
-        <Button>Button text</Button>
-        <Button size="medium" type="secondary">
-          Button text
-        </Button>
-        <Button size="medium" type="default">
-          Button text
-        </Button>
-        <Button size="medium" type="link">
-          Button text
-        </Button>
-        <Button size="medium" type="text">
-          Button text
-        </Button>
-        <Button size="medium" type="dashed">
-          Button text
-        </Button>
-        <Button size="medium" type="outline">
-          Button text
-        </Button>
-      </Space>
-
-      <Space>
-        <Button size="large">Button text</Button>
-        <Button size="large" type="secondary">
-          Button text
-        </Button>
-        <Button size="large" type="default">
-          Button text
-        </Button>
-        <Button size="large" type="link">
-          Button text
-        </Button>
-        <Button size="large" type="text">
-          Button text
-        </Button>
-        <Button size="large" type="dashed">
-          Button text
-        </Button>
-        <Button size="large" type="outline">
-          Button text
-        </Button>
-      </Space>
-      <Space>
-        <Button size="xlarge">Button text</Button>
-        <Button size="xlarge" type="secondary">
-          Button text
-        </Button>
-        <Button size="xlarge" type="default">
-          Button text
-        </Button>
-        <Button size="xlarge" type="link">
-          Button text
-        </Button>
-        <Button size="xlarge" type="text">
-          Button text
-        </Button>
-        <Button size="xlarge" type="dashed">
-          Button text
-        </Button>
-        <Button size="xlarge" type="outline">
-          Button text
-        </Button>
-      </Space>
-    </Space>
-  </>
-)
-export const Default = (args: any) => <Button>Button text</Button>
-export const withStyles = (args: any) => <Button>Button text</Button>
-export const withIcon = (args: any) => <Button>Button text</Button>
+export const Default = (args: any) => <Button {...args}>Button text</Button>
+export const withStyles = (args: any) => <Button {...args}>Button text</Button>
+export const withIcon = (args: any) => <Button {...args}>Button text</Button>
 export const withIconRight = (args: any) => (
   <Button {...args}>Button text</Button>
 )
