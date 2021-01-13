eb34b45e1a0d19db51e28325c0ce46ff513d8a8f
mildtomato <jon.summers.muir@googlemail.com>
2021-01-13 18:48:02 +0800

    add new examples of <Modal>

--- src/components/Modal/Modal.stories.js
+++ src/components/Modal/Modal.stories.js
@@ -4,6 +4,9 @@ import { action } from '@storybook/addon-actions'
 
 import { Modal } from '.'
 import Typography from '../Typography'
+import { Badge } from '../Badge'
+import { Button } from '../Button'
+import { Space } from '../Space'
 
 export default {
   title: 'Overlays/Modal',
@@ -39,6 +42,55 @@ export const withIcon = (args) => (
     onConfirm={action('onConfirm')}
     title="This is the title of the modal"
     description="And i am the description"
+  >
+    <Typography.Text>
+      Modal content is inserted here, if you need to insert anything into the
+      Modal you can do so via{' '}
+      <Typography.Text code>{'{children}'}</Typography.Text>
+    </Typography.Text>
+  </Modal>
+)
+
+export const hideFooter = (args) => (
+  <Modal
+    {...args}
+    visible={true}
+    showIcon={true}
+    hideFooter={true}
+    className="font-sans"
+    onCancel={action('onCancel')}
+    onConfirm={action('onConfirm')}
+    title="This is the title of the modal"
+    description="And i am the description"
+  >
+    <Typography.Text>
+      Modal content is inserted here, if you need to insert anything into the
+      Modal you can do so via{' '}
+      <Typography.Text code>{'{children}'}</Typography.Text>
+    </Typography.Text>
+  </Modal>
+)
+
+export const customFooter = (args) => (
+  <Modal
+    {...args}
+    visible={true}
+    className="font-sans"
+    onCancel={action('onCancel')}
+    onConfirm={action('onConfirm')}
+    title="This is the title of the modal"
+    description="And i am the description"
+    customFooter={[
+      <Space>
+        <div>
+          <Badge color="red" dot size="small">
+            Proceed with caution
+          </Badge>
+        </div>
+        <Button type="secondary">Cancel</Button>
+        <Button danger>Delete</Button>
+      </Space>,
+    ]}
   >
     <Typography.Text>
       Modal content is inserted here, if you need to insert anything into the
