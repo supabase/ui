81e8ca52254ac3498306baf093e7f91e2bbe994f
mildtomato <jon.summers.muir@googlemail.com>
2021-01-18 19:15:52 +0800

    feat: Divider updated to also contain text, aligned either `left`, `right` or `center`, using `orientation` prop. also added a `light` prop, useful for showing more subtle divider lines.

---
+++ src/components/Divider/Divider.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { Divider } from './'
+
+export default {
+  title: 'Utilities/Divider',
+  component: Divider,
+}
+
+export const Default = (args: any) => <Divider {...args} />
+
+export const withCenterText = (args: any) => (
+  <Divider {...args}>Hello world</Divider>
+)
+
+export const withLeftText = (args: any) => (
+  <Divider {...args}>Hello world</Divider>
+)
+
+export const withRightText = (args: any) => (
+  <Divider {...args}>Hello world</Divider>
+)
+
+export const lighterColor = (args: any) => <Divider {...args} />
+
+Default.args = {}
+
+withCenterText.args = {}
+
+withLeftText.args = {
+  orientation: 'left',
+  light: true,
+}
+
+withRightText.args = {
+  orientation: 'right',
+}
+
+lighterColor.args = {
+  light: true,
+}
--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,6 +1,36 @@
 import React from 'react'
 import './Divider.css'
 
-export default function Divider() {
-  return <div className="sbui-divider"></div>
+interface Props {
+  children?: React.ReactNode
+  className?: string
+  light?: boolean
+  orientation?: 'left' | 'right' | 'center'
+  style?: React.CSSProperties
+}
+
+export default function Divider({
+  children,
+  className,
+  light = false,
+  orientation = 'center',
+  style,
+}: Props) {
+  let classes = ['sbui-divider']
+  if (light) {
+    classes.push(`sbui-divider--light`)
+  }
+  if (children) {
+    classes.push(`sbui-divider--${orientation}`)
+  } else {
+    classes.push(`sbui-divider--no-text`)
+  }
+
+  if (className) classes.push(className)
+
+  return (
+    <div className={classes.join(' ')} role="seperator" style={style}>
+      {children && <span className="sbui-divider__content">{children}</span>}
+    </div>
+  )
 }
