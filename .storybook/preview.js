bbbb8daa6d50173e0a61a5ac6b88f724bc3691f4
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-02-03 16:25:39 +0800

    fix: storybook background issues fixed

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,12 @@
-
 // import css for san serif font styling
 import './preview.css'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   darkMode: {
+    current: 'dark',
     darkClass: 'dark',
     lightClass: 'light',
-    stylePreview: true
-  }
+    stylePreview: true,
+  },
 }
