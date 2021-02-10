32766c4d76dcf9e81a090680efcb740556777c3e
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-02-10 18:05:41 +0800

    fix: allow for view overriding | fixes #122

--- src/components/Auth/Auth.stories.tsx
+++ src/components/Auth/Auth.stories.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-// import { AutoForm } from 'uniforms'
-
+import React, { useState } from 'react'
 import { Auth } from './'
 import { createClient } from '@supabase/supabase-js'
 // @ts-ignore
-import { Typography, Button } from '../../index'
+import { Typography, Button, Space } from '../../index'
 
 const supabase = createClient(
   'https://evuqlpfsuimdzxurpcgn.supabase.co',
@@ -73,6 +71,48 @@ export const withSocialAuthHorizontal = (args: any) => (
   </Auth.UserContextProvider>
 )
 export const updatePassword = (args: any) => <Auth.UpdatePassword {...args} />
+
+export const ChangeViewState = (args: any) => {
+  const [view, setView] = useState<
+    'sign_in' | 'sign_up' | 'forgotten_password' | 'magic_link'
+  >('sign_in')
+
+  return (
+    <div>
+      <Space>
+        <Button
+          type={view === 'sign_up' ? 'primary' : 'default'}
+          onClick={() => setView('sign_up')}
+        >
+          Sign up
+        </Button>
+        <Button
+          type={view === 'sign_in' ? 'primary' : 'default'}
+          onClick={() => setView('sign_in')}
+        >
+          Sign in
+        </Button>
+        <Button
+          type={view === 'forgotten_password' ? 'primary' : 'default'}
+          onClick={() => setView('forgotten_password')}
+        >
+          Forgotten password
+        </Button>
+        <Button
+          type={view === 'magic_link' ? 'primary' : 'default'}
+          onClick={() => setView('magic_link')}
+        >
+          Magic link
+        </Button>
+      </Space>
+      <Auth.UserContextProvider supabaseClient={supabase}>
+        <Container supabaseClient={supabase}>
+          <Auth supabaseClient={supabase} view={view} />
+        </Container>
+      </Auth.UserContextProvider>
+    </div>
+  )
+}
 
 Default.args = {
   supabaseClient: supabase,
