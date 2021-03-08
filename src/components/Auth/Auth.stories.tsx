9ae64bd5ae3a00e242b1fd5f38e2f85fefeca27f
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-03-08 22:54:24 +0800

    chore: change supabase Auth project

--- src/components/Auth/Auth.stories.tsx
+++ src/components/Auth/Auth.stories.tsx
@@ -5,8 +5,8 @@ import { createClient } from '@supabase/supabase-js'
 import { Typography, Button, Space } from '../../index'
 
 const supabase = createClient(
-  'https://evuqlpfsuimdzxurpcgn.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYwNDIzODk3MywiZXhwIjoxOTE5ODE0OTczfQ.ud4NW5ZFc0Zky-ARnOzbzxqvLcYwVIyvk3GwW3aKC3Y'
+  'https://rsnibhkhsbfnncjmwnkj.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIxNDE1MywiZXhwIjoxOTMwNzkwMTUzfQ.OQEbAaTfgDdLCCht251P2JRD3QDnui6nsU8N-tZA_Mc'
 )
 
 export default {
