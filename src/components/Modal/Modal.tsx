d4622649e8fbb277c1b5bf2403e2558455b4765b
Jonathan Summers-Muir <jon.summers.muir@googlemail.com>
2021-04-15 16:17:24 +0800

    feat: added HeadlessUI transition and adopted Modal component to use it

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 // @ts-ignore
 import ModalStyles from './Modal.module.css'
-import { Button, Transition, IconX, Typography, Space } from './../../index'
+import { Button, IconX, Typography, Space } from './../../index'
 import { AnimationTailwindClasses } from '../../types'
+
+import { Transition } from '@headlessui/react'
+
+// import { Transition } from '@tailwindui/react'
 
 interface Props {
   children?: React.ReactNode
@@ -79,6 +83,8 @@ const Modal = ({
   let overlayClasses = [ModalStyles['sbui-modal-overlay']]
   if (overlayClassName) overlayClasses.push(overlayClassName)
 
+  console.log('onConfirmText', onConfirmText)
+
   const footerContent = customFooter ? (
     customFooter
   ) : (
@@ -111,107 +117,107 @@ const Modal = ({
   )
 
   return (
-    <Transition
-      show={visible}
-      transition={transitionOverlay}
-      enter={ModalStyles[`sbui-modal-overlay--enter`]}
-      enterFrom={ModalStyles[`sbui-modal-overlay--enterFrom`]}
-      enterTo={ModalStyles[`sbui-modal-overlay--enterTo`]}
-      leave={ModalStyles[`sbui-modal-overlay--leave`]}
-      leaveFrom={ModalStyles[`sbui-modal-overlay--leaveFrom`]}
-      leaveTo={ModalStyles[`sbui-modal-overlay--leaveTo`]}
-    >
-      <div
-        className={ModalStyles['sbui-modal-container'] + ' ' + className}
-        onClick={() => (onCancel ? onCancel() : null)}
+    <Transition show={visible}>
+      <Transition.Child
+        enter={ModalStyles[`sbui-modal-overlay--enter`]}
+        enterFrom={ModalStyles[`sbui-modal-overlay--enterFrom`]}
+        enterTo={ModalStyles[`sbui-modal-overlay--enterTo`]}
+        leave={ModalStyles[`sbui-modal-overlay--leave`]}
+        leaveFrom={ModalStyles[`sbui-modal-overlay--leaveFrom`]}
+        leaveTo={ModalStyles[`sbui-modal-overlay--leaveTo`]}
       >
-        <div className={ModalStyles['sbui-modal-flex-container']}>
-          <div className={ModalStyles['sbui-modal-overlay-container']}>
-            <div
-              className={overlayClasses.join(' ')}
-              style={overlayStyle}
-            ></div>
-          </div>
-          {/* <!-- This element is to trick the browser into centering the modal contents. --> */}
-          <span className={ModalStyles['sbui-modal-div-trick']}></span>
-          <Transition
-            show={visible}
-            transition={transition}
-            enter={ModalStyles[`sbui-modal--enter`]}
-            enterFrom={ModalStyles[`sbui-modal--enterFrom`]}
-            enterTo={ModalStyles[`sbui-modal--enterTo`]}
-            leave={ModalStyles[`sbui-modal--leave`]}
-            leaveFrom={ModalStyles[`sbui-modal--leaveFrom`]}
-            leaveTo={ModalStyles[`sbui-modal--leaveTo`]}
-          >
-            <div
-              className={modalClasses.join(' ')}
-              role="dialog"
-              aria-modal="true"
-              aria-labelledby="modal-headline"
-              onClick={stopPropagation}
-              style={style}
+        <div
+          className={ModalStyles['sbui-modal-container'] + ' ' + className}
+          onClick={() => (onCancel ? onCancel() : null)}
+        >
+          <div className={ModalStyles['sbui-modal-flex-container']}>
+            <div className={ModalStyles['sbui-modal-overlay-container']}>
+              <div
+                className={overlayClasses.join(' ')}
+                style={overlayStyle}
+              ></div>
+            </div>
+            {/* <!-- This element is to trick the browser into centering the modal contents. --> */}
+            <span className={ModalStyles['sbui-modal-div-trick']}></span>
+            <Transition.Child
+              enter={ModalStyles[`sbui-modal--enter`]}
+              enterFrom={ModalStyles[`sbui-modal--enterFrom`]}
+              enterTo={ModalStyles[`sbui-modal--enterTo`]}
+              leave={ModalStyles[`sbui-modal--leave`]}
+              leaveFrom={ModalStyles[`sbui-modal--leaveFrom`]}
+              leaveTo={ModalStyles[`sbui-modal--leaveTo`]}
+              className="fixed inset-0"
             >
               <div
-                className={ModalStyles['sbui-modal-content']}
-                style={contentStyle}
+                className={modalClasses.join(' ')}
+                role="dialog"
+                aria-modal="true"
+                aria-labelledby="modal-headline"
+                onClick={stopPropagation}
+                style={style}
               >
-                <Space
-                  size={5}
-                  style={{
-                    alignItems: layout === 'vertical' ? 'center' : 'flex-start',
-                  }}
-                  direction={layout}
+                <div
+                  className={ModalStyles['sbui-modal-content']}
+                  style={contentStyle}
                 >
-                  {icon ? icon : null}
                   <Space
-                    size={4}
-                    direction="vertical"
+                    size={5}
                     style={{
-                      alignItems: 'flex-start',
-                      textAlign: layout === 'vertical' ? 'center' : null,
-                      width: '100%',
+                      alignItems:
+                        layout === 'vertical' ? 'center' : 'flex-start',
                     }}
+                    direction={layout}
                   >
-                    <span style={{ width: 'inherit' }}>
-                      {title && (
-                        <Typography.Title
-                          style={{
-                            marginBottom: '.1rem',
-                            marginTop: '0',
-                          }}
-                          level={4}
-                        >
-                          {title}
-                        </Typography.Title>
-                      )}
-                      {description && (
-                        <Typography.Text>{description}</Typography.Text>
-                      )}
-                    </span>
+                    {icon ? icon : null}
+                    <Space
+                      size={4}
+                      direction="vertical"
+                      style={{
+                        alignItems: 'flex-start',
+                        textAlign: layout === 'vertical' ? 'center' : null,
+                        width: '100%',
+                      }}
+                    >
+                      <span style={{ width: 'inherit' }}>
+                        {title && (
+                          <Typography.Title
+                            style={{
+                              marginBottom: '.1rem',
+                              marginTop: '0',
+                            }}
+                            level={4}
+                          >
+                            {title}
+                          </Typography.Title>
+                        )}
+                        {description && (
+                          <Typography.Text>{description}</Typography.Text>
+                        )}
+                      </span>
 
-                    {children}
-                    {!footerBackground && !hideFooter && footerContent}
+                      {children}
+                      {!footerBackground && !hideFooter && footerContent}
+                    </Space>
                   </Space>
-                </Space>
-              </div>
-              {!hideFooter && footerBackground && (
-                <div className={footerClasses.join(' ')}>{footerContent}</div>
-              )}
-              {closable && (
-                <div className={ModalStyles['sbui-modal-close-container']}>
-                  <Button
-                    onClick={onCancel}
-                    type="text"
-                    shadow={false}
-                    icon={<IconX size="medium" />}
-                  />
                 </div>
-              )}
-            </div>
-          </Transition>
+                {!hideFooter && footerBackground && (
+                  <div className={footerClasses.join(' ')}>{footerContent}</div>
+                )}
+                {closable && (
+                  <div className={ModalStyles['sbui-modal-close-container']}>
+                    <Button
+                      onClick={onCancel}
+                      type="text"
+                      shadow={false}
+                      icon={<IconX size="medium" />}
+                    />
+                  </div>
+                )}
+              </div>
+            </Transition.Child>
+          </div>
         </div>
-      </div>
+      </Transition.Child>
     </Transition>
   )
 }
--- src/components/Transition/Transition.tsx
+++ src/components/Transition/Transition.tsx
@@ -1,142 +1,143 @@
 // modified, unhooked version
 
-import React from 'react'
-// @ts-ignore
-import { CSSTransition as ReactCSSTransition } from 'react-transition-group'
-import PropTypes from 'prop-types'
-import { AnimationTailwindClasses } from './../../types'
+// import React from 'react'
+// // @ts-ignore
+// import { CSSTransition as ReactCSSTransition } from 'react-transition-group'
+// import PropTypes from 'prop-types'
+// import { AnimationTailwindClasses } from './../../types'
 
-// function useIsInitialRender() {
-//   const isInitialRender = useRef(true)
-//   useEffect(() => {
-//     isInitialRender.current = false
-//   }, [])
-//   return isInitialRender.current
+// // function useIsInitialRender() {
+// //   const isInitialRender = useRef(true)
+// //   useEffect(() => {
+// //     isInitialRender.current = false
+// //   }, [])
+// //   return isInitialRender.current
+// // }
+
+// function CSSTransition({
+//   show,
+//   enter = '',
+//   enterFrom = '',
+//   enterTo = '',
+//   leave = '',
+//   leaveFrom = '',
+//   leaveTo = '',
+//   appear,
+//   children,
+// }: any) {
+//   const enterClasses = enter.split(' ').filter((s: any) => s.length)
+//   const enterFromClasses = enterFrom.split(' ').filter((s: any) => s.length)
+//   const enterToClasses = enterTo.split(' ').filter((s: any) => s.length)
+//   const leaveClasses = leave.split(' ').filter((s: any) => s.length)
+//   const leaveFromClasses = leaveFrom.split(' ').filter((s: any) => s.length)
+//   const leaveToClasses = leaveTo.split(' ').filter((s: any) => s.length)
+
+//   function addClasses(node: any, classes: any) {
+//     classes.length && node.classList.add(...classes)
+//   }
+
+//   function removeClasses(node: any, classes: any) {
+//     classes.length && node.classList.remove(...classes)
+//   }
+
+//   return (
+//     <ReactCSSTransition
+//       appear={appear}
+//       unmountOnExit
+//       in={show}
+//       addEndListener={(node: any, done: any) => {
+//         node.addEventListener('transitionend', done, false)
+//       }}
+//       onEnter={(node: any) => {
+//         addClasses(node, [...enterClasses, ...enterFromClasses])
+//       }}
+//       onEntering={(node: any) => {
+//         removeClasses(node, enterFromClasses)
+//         addClasses(node, enterToClasses)
+//       }}
+//       onEntered={(node: any) => {
+//         removeClasses(node, [...enterToClasses, ...enterClasses])
+//       }}
+//       onExit={(node: any) => {
+//         addClasses(node, [...leaveClasses, ...leaveFromClasses])
+//       }}
+//       onExiting={(node: any) => {
+//         removeClasses(node, leaveFromClasses)
+//         addClasses(node, leaveToClasses)
+//       }}
+//       onExited={(node: any) => {
+//         removeClasses(node, [...leaveToClasses, ...leaveClasses])
+//       }}
+//     >
+//       {children}
+//     </ReactCSSTransition>
+//   )
 // }
 
-function CSSTransition({
-  show,
-  enter = '',
-  enterFrom = '',
-  enterTo = '',
-  leave = '',
-  leaveFrom = '',
-  leaveTo = '',
-  appear,
-  children,
-}: any) {
-  const enterClasses = enter.split(' ').filter((s: any) => s.length)
-  const enterFromClasses = enterFrom.split(' ').filter((s: any) => s.length)
-  const enterToClasses = enterTo.split(' ').filter((s: any) => s.length)
-  const leaveClasses = leave.split(' ').filter((s: any) => s.length)
-  const leaveFromClasses = leaveFrom.split(' ').filter((s: any) => s.length)
-  const leaveToClasses = leaveTo.split(' ').filter((s: any) => s.length)
+// function TransitionCompiler({ show, appear, ...rest }: any) {
+//   return <CSSTransition appear={appear} show={show} {...rest} />
+// }
 
-  function addClasses(node: any, classes: any) {
-    classes.length && node.classList.add(...classes)
-  }
+// interface TransitionProps {
+//   show: Boolean
+//   children?: React.ReactNode
+//   // supabase ui animation
+//   enter?: string
+//   enterFrom?: string
+//   enterTo?: string
+//   leave?: string
+//   leaveFrom?: string
+//   leaveTo?: string
+//   // custom animation (tailwind based)
+//   transition?: AnimationTailwindClasses
+// }
 
-  function removeClasses(node: any, classes: any) {
-    classes.length && node.classList.remove(...classes)
-  }
+// const Transition = (props: TransitionProps) => {
+//   return (
+//     <TransitionCompiler
+//       show={props.show}
+//       enter={
+//         props.transition && props.transition.enter
+//           ? props.transition.enter
+//           : props.enter
+//       }
+//       enterFrom={
+//         props.transition && props.transition.enterFrom
+//           ? props.transition.enterFrom
+//           : props.enterFrom
+//       }
+//       enterTo={
+//         props.transition && props.transition.enterTo
+//           ? props.transition.enterTo
+//           : props.enterTo
+//       }
+//       leave={
+//         props.transition && props.transition.leave
+//           ? props.transition.leave
+//           : props.leave
+//       }
+//       leaveFrom={
+//         props.transition && props.transition.leaveFrom
+//           ? props.transition.leaveFrom
+//           : props.leaveFrom
+//       }
+//       leaveTo={
+//         props.transition && props.transition.leaveTo
+//           ? props.transition.leaveTo
+//           : props.leaveTo
+//       }
+//     >
+//       {props.children}
+//     </TransitionCompiler>
+//   )
+// }
 
-  return (
-    <ReactCSSTransition
-      appear={appear}
-      unmountOnExit
-      in={show}
-      addEndListener={(node: any, done: any) => {
-        node.addEventListener('transitionend', done, false)
-      }}
-      onEnter={(node: any) => {
-        addClasses(node, [...enterClasses, ...enterFromClasses])
-      }}
-      onEntering={(node: any) => {
-        removeClasses(node, enterFromClasses)
-        addClasses(node, enterToClasses)
-      }}
-      onEntered={(node: any) => {
-        removeClasses(node, [...enterToClasses, ...enterClasses])
-      }}
-      onExit={(node: any) => {
-        addClasses(node, [...leaveClasses, ...leaveFromClasses])
-      }}
-      onExiting={(node: any) => {
-        removeClasses(node, leaveFromClasses)
-        addClasses(node, leaveToClasses)
-      }}
-      onExited={(node: any) => {
-        removeClasses(node, [...leaveToClasses, ...leaveClasses])
-      }}
-    >
-      {children}
-    </ReactCSSTransition>
-  )
-}
-
-function TransitionCompiler({ show, appear, ...rest }: any) {
-  return <CSSTransition appear={appear} show={show} {...rest} />
-}
-
-interface TransitionProps {
-  show: Boolean
-  children?: React.ReactNode
-  // supabase ui animation
-  enter?: string
-  enterFrom?: string
-  enterTo?: string
-  leave?: string
-  leaveFrom?: string
-  leaveTo?: string
-  // custom animation (tailwind based)
-  transition?: AnimationTailwindClasses
-}
-
-const Transition = (props: TransitionProps) => {
-  return (
-    <TransitionCompiler
-      show={props.show}
-      enter={
-        props.transition && props.transition.enter
-          ? props.transition.enter
-          : props.enter
-      }
-      enterFrom={
-        props.transition && props.transition.enterFrom
-          ? props.transition.enterFrom
-          : props.enterFrom
-      }
-      enterTo={
-        props.transition && props.transition.enterTo
-          ? props.transition.enterTo
-          : props.enterTo
-      }
-      leave={
-        props.transition && props.transition.leave
-          ? props.transition.leave
-          : props.leave
-      }
-      leaveFrom={
-        props.transition && props.transition.leaveFrom
-          ? props.transition.leaveFrom
-          : props.leaveFrom
-      }
-      leaveTo={
-        props.transition && props.transition.leaveTo
-          ? props.transition.leaveTo
-          : props.leaveTo
-      }
-    >
-      {props.children}
-    </TransitionCompiler>
-  )
-}
-
-export default Transition
+// export default Transition
 
 // // From: https://gist.github.com/adamwathan/3b9f3ad1a285a2d1b482769aeb862467
 
 // import React, { useRef, useEffect, useContext } from 'react'
+// // @ts-ignore
 // import { CSSTransition as ReactCSSTransition } from 'react-transition-group'
 // import PropTypes from 'prop-types'
 
@@ -162,19 +163,19 @@ export default Transition
 //   leaveTo = '',
 //   appear,
 //   children,
-// }) {
-//   const enterClasses = enter.split(' ').filter((s) => s.length)
-//   const enterFromClasses = enterFrom.split(' ').filter((s) => s.length)
-//   const enterToClasses = enterTo.split(' ').filter((s) => s.length)
-//   const leaveClasses = leave.split(' ').filter((s) => s.length)
-//   const leaveFromClasses = leaveFrom.split(' ').filter((s) => s.length)
-//   const leaveToClasses = leaveTo.split(' ').filter((s) => s.length)
+// }: any) {
+//   const enterClasses = enter.split(' ').filter((s: any) => s.length)
+//   const enterFromClasses = enterFrom.split(' ').filter((s: any) => s.length)
+//   const enterToClasses = enterTo.split(' ').filter((s: any) => s.length)
+//   const leaveClasses = leave.split(' ').filter((s: any) => s.length)
+//   const leaveFromClasses = leaveFrom.split(' ').filter((s: any) => s.length)
+//   const leaveToClasses = leaveTo.split(' ').filter((s: any) => s.length)
 
-//   function addClasses(node, classes) {
+//   function addClasses(node: any, classes: any) {
 //     classes.length && node.classList.add(...classes)
 //   }
 
-//   function removeClasses(node, classes) {
+//   function removeClasses(node: any, classes: any) {
 //     classes.length && node.classList.remove(...classes)
 //   }
 
@@ -183,27 +184,27 @@ export default Transition
 //       appear={appear}
 //       unmountOnExit
 //       in={show}
-//       addEndListener={(node, done) => {
+//       addEndListener={(node: any, done: any) => {
 //         node.addEventListener('transitionend', done, false)
 //       }}
-//       onEnter={(node) => {
+//       onEnter={(node: any) => {
 //         addClasses(node, [...enterClasses, ...enterFromClasses])
 //       }}
-//       onEntering={(node) => {
+//       onEntering={(node: any) => {
 //         removeClasses(node, enterFromClasses)
 //         addClasses(node, enterToClasses)
 //       }}
-//       onEntered={(node) => {
+//       onEntered={(node: any) => {
 //         removeClasses(node, [...enterToClasses, ...enterClasses])
 //       }}
-//       onExit={(node) => {
+//       onExit={(node: any) => {
 //         addClasses(node, [...leaveClasses, ...leaveFromClasses])
 //       }}
-//       onExiting={(node) => {
+//       onExiting={(node: any) => {
 //         removeClasses(node, leaveFromClasses)
 //         addClasses(node, leaveToClasses)
 //       }}
-//       onExited={(node) => {
+//       onExited={(node: any) => {
 //         removeClasses(node, [...leaveToClasses, ...leaveClasses])
 //       }}
 //     >
@@ -212,7 +213,7 @@ export default Transition
 //   )
 // }
 
-// function TransitionCompiler({ show, appear, ...rest }) {
+// function TransitionCompiler({ show, appear, ...rest }: any) {
 //   const { parent } = useContext(TransitionContext)
 //   const isInitialRender = useIsInitialRender()
 //   const isChild = show === undefined
@@ -220,7 +221,9 @@ export default Transition
 //   if (isChild) {
 //     return (
 //       <CSSTransition
+//         // @ts-ignore
 //         appear={parent.appear || !parent.isInitialRender}
+//         // @ts-ignore
 //         show={parent.show}
 //         {...rest}
 //       />
@@ -275,3 +278,136 @@ export default Transition
 // }
 
 // export default Transition
+
+// JSX Version by Adam Wathan: https://gist.github.com/adamwathan/e0a791aa0419098a7ece70028b2e641e
+
+import React, { ReactNode, useRef, useEffect, useContext } from 'react'
+import { CSSTransition as ReactCSSTransition } from 'react-transition-group'
+
+interface TransitionProps {
+  show?: boolean
+  enter?: string
+  enterFrom?: string
+  enterTo?: string
+  leave?: string
+  leaveFrom?: string
+  leaveTo?: string
+  appear?: string | boolean
+  className?: string
+  children: ReactNode
+}
+
+interface ParentContextProps {
+  parent: {
+    show?: boolean
+    appear?: string | boolean
+    isInitialRender?: boolean
+  }
+}
+
+const TransitionContext = React.createContext<ParentContextProps>({
+  parent: {},
+})
+
+function useIsInitialRender() {
+  const isInitialRender = useRef(true)
+  useEffect(() => {
+    isInitialRender.current = false
+  }, [])
+  return isInitialRender.current
+}
+
+function CSSTransition({
+  show,
+  enter = '',
+  enterFrom = '',
+  enterTo = '',
+  leave = '',
+  leaveFrom = '',
+  leaveTo = '',
+  appear,
+  className,
+  children,
+}: TransitionProps) {
+  const nodeRef = React.useRef<HTMLDivElement>(null)
+  const enterClasses = enter.split(' ').filter((s) => s.length)
+  const enterFromClasses = enterFrom.split(' ').filter((s) => s.length)
+  const enterToClasses = enterTo.split(' ').filter((s) => s.length)
+  const leaveClasses = leave.split(' ').filter((s) => s.length)
+  const leaveFromClasses = leaveFrom.split(' ').filter((s) => s.length)
+  const leaveToClasses = leaveTo.split(' ').filter((s) => s.length)
+
+  function addClasses(classes: string[]) {
+    if (nodeRef.current) nodeRef.current.classList.add(...classes)
+  }
+
+  function removeClasses(classes: string[]) {
+    if (nodeRef.current) nodeRef.current.classList.remove(...classes)
+  }
+
+  return (
+    <ReactCSSTransition
+      appear={appear}
+      unmountOnExit
+      in={show}
+      nodeRef={nodeRef}
+      addEndListener={(done) => {
+        nodeRef.current?.addEventListener('transitionend', done, false)
+      }}
+      onEnter={() => {
+        addClasses([...enterClasses, ...enterFromClasses])
+      }}
+      onEntering={() => {
+        removeClasses(enterFromClasses)
+        addClasses(enterToClasses)
+      }}
+      onEntered={() => {
+        removeClasses([...enterToClasses, ...enterClasses])
+      }}
+      onExit={() => {
+        addClasses([...leaveClasses, ...leaveFromClasses])
+      }}
+      onExiting={() => {
+        removeClasses(leaveFromClasses)
+        addClasses(leaveToClasses)
+      }}
+      onExited={() => {
+        removeClasses([...leaveToClasses, ...leaveClasses])
+      }}
+    >
+      <div ref={nodeRef} className={className}>
+        {children}
+      </div>
+    </ReactCSSTransition>
+  )
+}
+
+export function Transition({ show, appear, ...rest }: TransitionProps) {
+  const { parent } = useContext(TransitionContext)
+  const isInitialRender = useIsInitialRender()
+  const isChild = show === undefined
+
+  if (isChild) {
+    return (
+      <CSSTransition
+        appear={parent.appear || !parent.isInitialRender}
+        show={parent.show}
+        {...rest}
+      />
+    )
+  }
+
+  return (
+    <TransitionContext.Provider
+      value={{
+        parent: {
+          show,
+          isInitialRender,
+          appear,
+        },
+      }}
+    >
+      <CSSTransition appear={appear} show={show} {...rest} />
+    </TransitionContext.Provider>
+  )
+}
