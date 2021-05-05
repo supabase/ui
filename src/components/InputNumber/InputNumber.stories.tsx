8c151103c5d9192c3532999a54a08c57c05ab3be
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-05-06 00:55:17 +0530

    added storybook style for inputNumber

--- src/components/InputNumber/InputNumber.stories.tsx
+++ src/components/InputNumber/InputNumber.stories.tsx
@@ -13,10 +13,30 @@ export default {
 
 export const Default = (args: any) => <InputNumber {...args} />
 
+export const ErrorState = (args: any) => <InputNumber {...args} />
+
+export const WithIcon = (args: any) => <InputNumber {...args} />
+
 Default.args = {
   label: 'Max of 100 and min of 0',
   disabled: false,
   layout: 'vertical',
   max: 100,
   min: 0,
+}
+
+ErrorState.args = {
+  disabled: false,
+  layout: 'vertical',
+  label: 'Input Number with an error message',
+  error: 'Input Number must be in range',
+}
+
+WithIcon.args = {
+  label: 'Max of 100 and min of 0 with a Icon',
+  disabled: false,
+  layout: 'vertical',
+  max: 100,
+  min: 0,
+  icon: <IconPackage />,
 }
