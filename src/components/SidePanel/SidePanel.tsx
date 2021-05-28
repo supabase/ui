6fbb641af4369823379af8782adedb094e26b274
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2021-05-28 15:52:29 +0800

    fix: SidePanel now accessible
    • will include a trigger prop to insert a trigger, which is wrapped in a button with correct aria labels
    • animation of side panel is fixed
    • using @radix-ui/react-dialog for the sidepanel (which we can also use for Modal)

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // @ts-ignore
 import SlidePanelStyles from './SidePanel.module.css'
 import { Button, IconX, Space, Typography } from '../../index'
 import { AnimationTailwindClasses } from '../../types'
+
+import * as Dialog from '@radix-ui/react-dialog'
 
 import { Transition } from '@headlessui/react'
 
@@ -45,6 +47,8 @@ const SidePanel = ({
   transition,
   transitionOverlay,
 }: Props) => {
+  const [open, setOpen] = React.useState(visible)
+
   function stopPropagation(e: React.MouseEvent) {
     e.stopPropagation()
   }
@@ -91,6 +95,118 @@ const SidePanel = ({
         </Button>
       </Space>
     </div>
+  )
+
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger>Open</Dialog.Trigger>
+      <Transition show={open}>
+        <Dialog.Overlay forceMount>
+          <Transition.Child
+            enter={SlidePanelStyles[`sbui-sidepanel-overlay--enter`]}
+            enterFrom={SlidePanelStyles[`sbui-sidepanel-overlay--enterFrom`]}
+            enterTo={SlidePanelStyles[`sbui-sidepanel-overlay--enterTo`]}
+            leave={SlidePanelStyles[`sbui-sidepanel-overlay--leave`]}
+            leaveFrom={SlidePanelStyles[`sbui-sidepanel-overlay--leaveFrom`]}
+            leaveTo={SlidePanelStyles[`sbui-sidepanel-overlay--leaveTo`]}
+          >
+            <div
+              className={SlidePanelStyles['sbui-sidepanel-overlay-container']}
+            >
+              <div className={SlidePanelStyles['sbui-sidepanel-overlay']}></div>
+            </div>
+          </Transition.Child>
+        </Dialog.Overlay>
+
+        <Dialog.Content forceMount style={{ width: '100vw' }}>
+          <div className={containerClasses.join(' ')}>
+            <Transition.Child
+              enter={SlidePanelStyles[`sbui-sidepanel--enter`]}
+              enterFrom={
+                left
+                  ? SlidePanelStyles[`sbui-sidepanel--enterFrom--left`]
+                  : SlidePanelStyles[`sbui-sidepanel--enterFrom`]
+              }
+              enterTo={SlidePanelStyles[`sbui-sidepanel--enterTo`]}
+              leave={SlidePanelStyles[`sbui-sidepanel--leave`]}
+              leaveFrom={SlidePanelStyles[`sbui-sidepanel--leaveFrom`]}
+              leaveTo={
+                left
+                  ? SlidePanelStyles[`sbui-sidepanel--leaveTo--left`]
+                  : SlidePanelStyles[`sbui-sidepanel--leaveTo`]
+              }
+            >
+              <div
+                className={
+                  wide
+                    ? SlidePanelStyles['sbui-sidepanel--wide']
+                    : SlidePanelStyles['sbui-sidepanel--medium']
+                }
+              >
+                <div
+                  className={sidePanelClasses.join(' ')}
+                  onClick={stopPropagation}
+                >
+                  <Space
+                    size={6}
+                    direction="vertical"
+                    style={{
+                      minHeight: '0',
+                      flex: '1 1 0%',
+                      overflowY: 'scroll',
+                    }}
+                  >
+                    <header
+                      className={SlidePanelStyles['sbui-sidepanel-header']}
+                    >
+                      <Space
+                        size={3}
+                        direction="row"
+                        style={{
+                          alignItems: 'center',
+                          justifyContent: 'space-between',
+                        }}
+                      >
+                        {title && (
+                          <Typography.Title className="m-0" level={4}>
+                            {title}
+                          </Typography.Title>
+                        )}
+                        <div
+                          className={
+                            SlidePanelStyles['sbui-sidepanel-close-container']
+                          }
+                        >
+                          <Button
+                            aria-label="Close panel"
+                            onClick={onCancel}
+                            type="text"
+                            shadow={false}
+                            style={{ padding: 0 }}
+                            icon={<IconX size="xlarge" strokeWidth={2} />}
+                          />
+                        </div>
+                      </Space>
+                      <div>
+                        {description && (
+                          <Typography.Text type="secondary">
+                            {description}
+                          </Typography.Text>
+                        )}
+                      </div>
+                    </header>
+                    <div className={SlidePanelStyles['sbui-sidepanel-content']}>
+                      {children}
+                    </div>
+                  </Space>
+                  {!hideFooter && footerContent}
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog.Content>
+      </Transition>
+    </Dialog.Root>
   )
 
   return (
