5fdda7e6d77c49e1ea34d79ebcb80470d09cb027
Jon Thompson <jon@jont.me>
2021-05-30 19:50:37 +0100

    Adds headlessui Disclousure

--- src/components/Accordian/Accordian.stories.tsx
+++ src/components/Accordian/Accordian.stories.tsx
@@ -7,6 +7,32 @@ export default {
   component: Accordian,
 }
 
-export const Single = (args: any) => <Accordian {...args} />
+export const Single = (args: any) => (
+  <Accordian {...args}>
+    <Accordian.Item label="Single Item">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        amet labore.
+      </div>
+    </Accordian.Item>
+  </Accordian>
+)
+
+export const Multiple = (args: any) => (
+  <Accordian {...args}>
+    <Accordian.Item label="1st Item">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        amet labore.
+      </div>
+    </Accordian.Item>
+    <Accordian.Item label="2nd Item">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        amet labore.
+      </div>
+    </Accordian.Item>
+  </Accordian>
+)
 
 Single.args = {}
--- src/components/Accordian/Accordian.tsx
+++ src/components/Accordian/Accordian.tsx
@@ -1,21 +1,42 @@
 import React from 'react'
-
+import { Disclosure } from '@headlessui/react'
 interface AccordianProps {
   children?: React.ReactNode
   className?: string
+  defaultActiveId?: string | number
+  expandedIcon?: any
+  expandIconPosition?: 'left' | 'right'
+  bordered?: boolean
 }
 
-function Accordian({ children, className }: AccordianProps) {
-  return <ul className={className}>{children}</ul>
+function Accordian({
+  children,
+  className,
+  defaultActiveId,
+  expandedIcon,
+  expandIconPosition,
+  bordered,
+}: AccordianProps) {
+  return (
+    <div className={className}>
+      <Disclosure>{({ open }) => <>{children}</>}</Disclosure>
+    </div>
+  )
 }
 
 interface ItemProps {
   children?: React.ReactNode
   className?: string
+  label: string
 }
 
-export function Item({ children, className }: ItemProps) {
-  return <li className={className}>{children}</li>
+export function Item({ children, className, label }: ItemProps) {
+  return (
+    <>
+      <Disclosure.Button>{label}</Disclosure.Button>
+      <Disclosure.Panel className={className}>{children}</Disclosure.Panel>
+    </>
+  )
 }
 
 Accordian.Item = Item
