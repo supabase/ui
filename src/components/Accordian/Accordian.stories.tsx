5c6df8f17c7351f3c687857f68091e1f9427e59a
Jon Thompson <jon@jont.me>
2021-05-31 19:15:33 +0100

    Adds stories for icon and iconPosition

--- src/components/Accordian/Accordian.stories.tsx
+++ src/components/Accordian/Accordian.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Accordian } from './'
 import { Typography } from '../Typography'
+import { IconArrowUp } from '../Icon/icons/IconArrowUp'
 
 export default {
   title: 'Displays/Accordian',
@@ -19,59 +20,44 @@ export const Default = (args: any) => (
   </Accordian>
 )
 
-export const withMultiple = (args: any) => (
-  <Accordian {...args}>
-    <Accordian.Item label="1st Item">
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        amet labore.
-      </Typography>
-    </Accordian.Item>
-    <Accordian.Item label="2nd Item">
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        amet labore.
-      </Typography>
-    </Accordian.Item>
-  </Accordian>
-)
-
-export const withBorder = (args: any) => (
-  <Accordian {...args}>
-    <Accordian.Item label="1st Item">
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        amet labore.
-      </Typography>
-    </Accordian.Item>
-    <Accordian.Item label="2nd Item">
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        amet labore.
-      </Typography>
-    </Accordian.Item>
-  </Accordian>
-)
-
-export const withDefaultActive = (args: any) => (
-  <Accordian {...args}>
-    <Accordian.Item label="Single Item" id={1}>
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        amet labore.
-      </Typography>
-    </Accordian.Item>
-  </Accordian>
-)
-
 Default.args = {}
 
+const Multiple = (args: any) => (
+  <Accordian {...args}>
+    <Accordian.Item label="1st Item">
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        amet labore.
+      </Typography>
+    </Accordian.Item>
+    <Accordian.Item label="2nd Item">
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        amet labore.
+      </Typography>
+    </Accordian.Item>
+  </Accordian>
+)
+
+export const withMultiple = Multiple.bind({})
 withMultiple.args = {}
 
+export const withBorder = Multiple.bind({})
 withBorder.args = {
   bordered: true,
 }
 
+export const withDefaultActive = Multiple.bind({})
 withDefaultActive.args = {
   defaultActiveId: 1,
+}
+
+export const withIconLeft = Multiple.bind({})
+withIconLeft.args = {
+  iconPosition: 'left',
+}
+
+export const withCustomIcon = Multiple.bind({})
+withCustomIcon.args = {
+  icon: <IconArrowUp />,
 }
