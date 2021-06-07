816c070f94a68508156a1e57a44ed1f4c2a50aff
Jon Thompson <jon@jont.me>
2021-06-07 19:12:32 +0100

    Adds as prop component types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,13 @@
+import type { ValidationMap } from 'prop-types'
+import type {
+  ComponentPropsWithoutRef,
+  ComponentPropsWithRef,
+  ComponentType,
+  ForwardedRef,
+  ReactElement,
+  WeakValidationMap,
+} from 'react'
+
 export interface AnimationTailwindClasses {
   enter?: string
   enterFrom?: string
@@ -5,4 +15,121 @@ export interface AnimationTailwindClasses {
   leave?: string
   leaveFrom?: string
   leaveTo?: string
+}
+
+export type WithElementProps<
+  Type extends keyof JSX.IntrinsicElements | ComponentType,
+  Props extends {}
+> = Omit<ComponentPropsWithoutRef<Type>, keyof Props> & Props
+
+export type AsPropsWithoutRef<
+  InitialType extends keyof JSX.IntrinsicElements | ComponentType,
+  Type extends
+    | keyof JSX.IntrinsicElements
+    | ComponentType
+    | undefined = InitialType
+> = {
+  as?: Type
+} & Omit<
+  Type extends undefined
+    ? ComponentPropsWithoutRef<InitialType>
+    : ComponentPropsWithoutRef<Type>,
+  'as'
+>
+
+export type AsPropsWithRef<
+  InitialType extends keyof JSX.IntrinsicElements | ComponentType,
+  Type extends
+    | keyof JSX.IntrinsicElements
+    | ComponentType
+    | undefined = InitialType
+> = {
+  as?: Type
+} & Omit<
+  Type extends undefined
+    ? ComponentPropsWithRef<InitialType>
+    : ComponentPropsWithRef<Type>,
+  'as'
+>
+export interface ComponentWithAsProps<
+  InitialType extends keyof JSX.IntrinsicElements | ComponentType<any>,
+  Props extends {}
+> {
+  <Type extends keyof JSX.IntrinsicElements>(
+    props: {
+      as: Type
+    } & Props &
+      Omit<AsPropsWithoutRef<InitialType, Type>, keyof Props>,
+    context?: any
+  ): ReactElement<any, any> | null
+  <Type extends ComponentType<any>>(
+    props: {
+      as: Type
+    } & Props &
+      Omit<AsPropsWithoutRef<InitialType, Type>, keyof Props>,
+    context?: any
+  ): ReactElement<any, any> | null
+  (
+    props: {
+      as?: InitialType
+    } & Props &
+      Omit<AsPropsWithoutRef<InitialType, InitialType>, keyof Props>,
+    context?: any
+  ): ReactElement<any, any> | null
+  (
+    props: {
+      as?: undefined
+    } & Props &
+      Omit<AsPropsWithoutRef<InitialType, undefined>, keyof Props>,
+    context?: any
+  ): ReactElement<any, any> | null
+  (
+    props: Props & Omit<AsPropsWithoutRef<InitialType, undefined>, keyof Props>,
+    context?: any
+  ): ReactElement<any, any> | null
+
+  propTypes?: WeakValidationMap<
+    Props & Omit<AsPropsWithoutRef<InitialType>, keyof Props>
+  >
+  contextTypes?: ValidationMap<any>
+  defaultProps?: Props & Omit<AsPropsWithoutRef<InitialType>, keyof Props>
+
+  displayName?: string
+}
+
+export interface RefForwardingComponentWithAsProps<
+  InitialType extends keyof JSX.IntrinsicElements | ComponentType<any>,
+  Props extends {}
+> {
+  <Type extends keyof JSX.IntrinsicElements>(
+    props: { as: Type } & Props &
+      Omit<AsPropsWithRef<InitialType, Type>, keyof Props>,
+    ref?: ForwardedRef<Type>
+  ): React.ReactElement | null
+  <Type extends ComponentType<any>>(
+    props: { as: Type } & Props &
+      Omit<AsPropsWithRef<InitialType, Type>, keyof Props>,
+    ref?: ForwardedRef<Type>
+  ): React.ReactElement | null
+  (
+    props: { as?: InitialType } & Props &
+      Omit<AsPropsWithRef<InitialType, InitialType>, keyof Props>,
+    ref?: ForwardedRef<InitialType>
+  ): React.ReactElement | null
+  (
+    props: { as?: undefined } & Props &
+      Omit<AsPropsWithRef<InitialType, undefined>, keyof Props>,
+    ref?: ForwardedRef<InitialType>
+  ): React.ReactElement | null
+  (
+    props: Props & Omit<AsPropsWithRef<InitialType, undefined>, keyof Props>,
+    ref?: ForwardedRef<InitialType>
+  ): React.ReactElement | null
+
+  propTypes?: WeakValidationMap<
+    Props & Omit<AsPropsWithRef<InitialType>, keyof Props>
+  >
+  contextTypes?: ValidationMap<any>
+  defaultProps?: Props & Omit<AsPropsWithRef<InitialType>, keyof Props>
+  displayName?: string
 }
