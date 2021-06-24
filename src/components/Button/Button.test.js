02fbe2da492467ddf296b39e4cff6b1213816e3b
Harsh Karande <66206863+harshcut@users.noreply.github.com>
2021-06-24 22:50:40 +0530

    test: render tests for button component

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Button from './Button'
 
 const SIZES = ['tiny', 'small', 'medium', 'large', 'xlarge']
@@ -14,9 +14,47 @@ const TYPES = [
 ]
 
 describe('#Button', () => {
-  it('should render button correctly', async () => {
-    render(<Button>Button</Button>)
-    expect(screen.queryByText('Button')).toBeInTheDocument()
+  it('should render button correctly', () => {
+    const wrapper = render(<Button>Button</Button>)
+    expect(wrapper.getByText('Button')).toBeInTheDocument()
+    expect(() => wrapper.unmount()).not.toThrow()
+  })
+
+  it('should render different text', () => {
+    const wrapper = render(<Button>Button</Button>)
+    expect(screen.getByText('Button')).toBeInTheDocument()
+    wrapper.rerender(<Button>按钮</Button>)
+    expect(screen.getByText('按钮')).toBeInTheDocument()
+  })
+  
+  it('should ignore events when disabled', () => {
+    const WrapperButton = () => {
+      const [state, setState] = React.useState('state1')
+      return (
+        <Button disabled onClick={() => setState('state2')}>
+          {state}
+        </Button>
+      )
+    }
+    render(<WrapperButton />)
+    expect(screen.getByText('state1')).toBeInTheDocument()
+    fireEvent.click(screen.getByText('state1'))
+    expect(screen.getByText('state1')).toBeInTheDocument()
+    expect(screen.queryByText('state2')).not.toBeInTheDocument()
+  })
+
+  it('should ignore events when loading', () => {
+    const WrapperButton = () => {
+      const [state, setState] = React.useState('state1')
+      return (
+        <Button loading onClick={() => setState('state2')}>
+          {state}
+        </Button>
+      )
+    }
+    render(<WrapperButton />)
+    fireEvent.click(screen.getByText('state1'))
+    expect(screen.queryByText('state2')).not.toBeInTheDocument()
   })
 
   it('should have "w-full" class', async () => {
