602b3ce5f5bb19320d882c48c45ce72e446026e9
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2021-10-22 23:08:57 +0800

    Chore: change button composition so it works more reliably as a submit button
    wasn't sure what was wrong here - required 2 clicks in a <form> for it to trigger an onSubmit 🤷‍♂️

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -130,53 +130,58 @@ const Button = forwardRef<RefHandle, ButtonProps>(
       return <Tag {...props} />
     }
 
-    const RenderedButton = ({ children }: any) =>
-      as ? (
-        <CustomButton
-          className={classes.join(' ')}
-          onClick={onClick}
-          style={style}
-        >
-          {children}
-        </CustomButton>
-      ) : (
-        <button
-          {...props}
-          ref={buttonRef}
-          className={classes.join(' ')}
-          disabled={loading || (disabled && true)}
-          onClick={onClick}
-          style={style}
-          type={htmlType}
-          aria-selected={ariaSelected}
-          aria-controls={ariaControls}
-          tabIndex={tabIndex}
-          role={role}
-        >
-          {children}
-        </button>
-      )
-
-    return (
-      <span ref={containerRef} className={containerClasses.join(' ')}>
-        <RenderedButton>
-          {showIcon &&
-            (loading ? (
-              <IconLoader size={size} className={iconLoaderClasses.join(' ')} />
-            ) : icon ? (
-              <IconContext.Provider value={{ contextSize: size }}>
-                {icon}
-              </IconContext.Provider>
-            ) : null)}
-          {children && <span>{children}</span>}
-          {iconRight && !loading && (
+    const buttonContent = (
+      <>
+        {showIcon &&
+          (loading ? (
+            <IconLoader size={size} className={iconLoaderClasses.join(' ')} />
+          ) : icon ? (
             <IconContext.Provider value={{ contextSize: size }}>
-              {iconRight}
+              {icon}
             </IconContext.Provider>
-          )}
-        </RenderedButton>
-      </span>
+          ) : null)}
+        {children && <span>{children}</span>}
+        {iconRight && !loading && (
+          <IconContext.Provider value={{ contextSize: size }}>
+            {iconRight}
+          </IconContext.Provider>
+        )}
+      </>
     )
+
+    if (as) {
+      return (
+        <span ref={containerRef} className={containerClasses.join(' ')}>
+          <CustomButton
+            className={classes.join(' ')}
+            onClick={onClick}
+            style={style}
+          >
+            {buttonContent}
+          </CustomButton>
+        </span>
+      )
+    } else {
+      return (
+        <span ref={containerRef} className={containerClasses.join(' ')}>
+          <button
+            {...props}
+            ref={buttonRef}
+            className={classes.join(' ')}
+            disabled={loading || (disabled && true)}
+            onClick={onClick}
+            style={style}
+            type={htmlType}
+            aria-selected={ariaSelected}
+            aria-controls={ariaControls}
+            tabIndex={tabIndex}
+            role={role}
+          >
+            {buttonContent}
+          </button>
+        </span>
+      )
+    }
   }
 )
 
